e7f655240de59c17ca1b7abba2fc773fb4b87d1a
EnigmaK9 <enigmak9@protonmail.com>
2023-02-12 12:56:01 -0600

    Study Drill of Exercise 5

---
+++ javahard/ex05/CreatingVariables.java
@@ -0,0 +1,32 @@
+public class CreatingVariables {
+    public static void main(String[] args) {
+    int x, y, age, weight;
+    double seconds, e, checking, height, grades;
+    String firstName, lastName, title, pronoun;
+
+    x = 10;
+    y = 400;
+    age = 39;
+    seconds = 4.71;
+    e = 2.71828182845904523536;
+    checking = 1.89;
+    height = 1.77;
+    weight = 82;
+    grades = 8.04;
+    pronoun = "He";
+
+    firstName = "Carlos";
+    lastName = " Padilla";
+    title = "Engineer";
+
+    System.out.println("The variable x contains " + x);
+    System.out.println("The value " + y + " is stored in the variable y.");
+    System.out.println("The experiment took " + seconds + " seconds.");
+    System.out.println("A favorite irrational # is Euler's number: " + e);
+    System.out.println("Hopefully you have more than $" + checking + "!" );
+    System.out.println("My name's " + title + " " + firstName + lastName );
+    System.out.println("My weight is " + weight + ", My height is " + height + ", My grades are "
+    + grades + ", my pronoun is " + pronoun );
+    }
+}
+
