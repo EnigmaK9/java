8ab83d47d2982690c526dca5a0694a9f6fcebf20
EnigmaK9 <enigmak9@protonmail.com>
2023-02-12 01:52:15 -0600

    added ex02

---
+++ javahard/ex02/GasolineReceipt.java
@@ -0,0 +1,15 @@
+public class GasolineReceipt {
+    public static void main( String[] args)  {
+           System.out.println("+---------------------------+");
+           System.out.println("|                           |");
+           System.out.println("|        CORNER STONE       |");
+           System.out.println("|                           |");
+           System.out.println("|2023-02-12      01:49      |");
+           System.out.println("|                           |");
+           System.out.println("| Gallons:       $ 2.089    |");
+           System.out.println("|                           |");
+           System.out.println("| Fuel Total:    $ 22.71    |");
+           System.out.println("|                           |");
+           System.out.println("+---------------------------+");
+    }
+}
